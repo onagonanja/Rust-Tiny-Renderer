31b6c7f05a5d2e9a4f460238a1d4232e389edf89
onagonanja <take-rei-7575@docomo.ne.jp>
2024-09-10 18:04:49 +0900

    use nalgebra

--- src/render.rs
+++ src/render.rs
@@ -1,66 +1,6 @@
 use image::{ImageBuffer, Rgb};
-use std::{
-    ops::{BitXor, Sub},
-    vec,
-};
+use nalgebra::{Vector2, Vector3};
 use tobj::Model;
-
-#[derive(Clone, Copy, Debug)]
-pub struct Pos2i {
-    x: i32,
-    y: i32,
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct Pos2f {
-    x: f32,
-    y: f32,
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct Pos3f {
-    x: f32,
-    y: f32,
-    z: f32,
-}
-
-impl Pos3f {
-    pub fn normalize(&self) -> Self {
-        let len = (self.x * self.x + self.y * self.y + self.z * self.z).sqrt();
-        Pos3f {
-            x: self.x / len,
-            y: self.y / len,
-            z: self.z / len,
-        }
-    }
-
-    pub fn dot(&self, rhs: Self) -> f32 {
-        self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
-    }
-}
-
-impl Sub for Pos3f {
-    type Output = Self;
-    fn sub(self, rhs: Self) -> Self {
-        Pos3f {
-            x: self.x - rhs.x,
-            y: self.y - rhs.y,
-            z: self.z - rhs.z,
-        }
-    }
-}
-
-// calculate cross product
-impl BitXor for Pos3f {
-    type Output = Self;
-    fn bitxor(self, rhs: Self) -> Self {
-        Pos3f {
-            x: self.y * rhs.z - self.z * rhs.y,
-            y: self.z * rhs.x - self.x * rhs.z,
-            z: self.x * rhs.y - self.y * rhs.x,
-        }
-    }
-}
 
 pub fn line(_x0: i32, _y0: i32, _x1: i32, _y1: i32, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>) {
     let (mut x0, mut x1, mut y0, mut y1) = (_x0, _x1, _y0, _y1);
@@ -100,99 +40,91 @@ pub fn line(_x0: i32, _y0: i32, _x1: i32, _y1: i32, image: &mut ImageBuffer<Rgb<
 }
 
 // calculate barycentric coordinates
-fn barycentric(pts: &[Pos3f; 3], p: Pos2i) -> Pos3f {
-    let u = Pos3f {
-        x: pts[2].x - pts[0].x,
-        y: pts[1].x - pts[0].x,
-        z: pts[0].x - p.x as f32,
-    };
-    let v = Pos3f {
-        x: pts[2].y - pts[0].y,
-        y: pts[1].y - pts[0].y,
-        z: pts[0].y - p.y as f32,
-    };
-    let cross = u ^ v;
+fn barycentric(pts: &[Vector3<f32>; 3], p: Vector2<f32>) -> Vector3<f32> {
+    let a = Vector3::new(
+        pts[2].x - pts[0].x,
+        pts[1].x - pts[0].x,
+        pts[0].x - p.x as f32,
+    );
+    let u = Vector3::new(
+        pts[2].x - pts[0].x,
+        pts[1].x - pts[0].x,
+        pts[0].x - p.x as f32,
+    );
+    let v = Vector3::new(
+        pts[2].y - pts[0].y,
+        pts[1].y - pts[0].y,
+        pts[0].y - p.y as f32,
+    );
+    let cross = u.cross(&v);
     if cross.z.abs() < 1.0 {
-        return Pos3f {
-            x: -1.0,
-            y: 1.0,
-            z: 1.0,
-        };
+        return Vector3::new(-1.0, 1.0, 1.0);
     }
-    Pos3f {
-        x: 1.0 - (cross.x + cross.y) / cross.z,
-        y: cross.y / cross.z,
-        z: cross.x / cross.z,
-    }
+    Vector3::new(
+        1.0 - (cross.x + cross.y) / cross.z,
+        cross.y / cross.z,
+        cross.x / cross.z,
+    )
 }
 
-fn get_world_coords(model: &Model, face: &[usize; 3]) -> [Pos3f; 3] {
-    let mut world_coords = [Pos3f {
-        x: 0.0,
-        y: 0.0,
-        z: 0.0,
-    }; 3];
+fn get_world_coords(model: &Model, face: &[usize; 3]) -> [Vector3<f32>; 3] {
+    let mut world_coords = [Vector3::new(0.0, 0.0, 0.0); 3];
     for i in 0..3 {
-        world_coords[i] = Pos3f {
-            x: model.mesh.positions[3 * face[i]],
-            y: model.mesh.positions[3 * face[i] + 1],
-            z: model.mesh.positions[3 * face[i] + 2],
-        };
+        world_coords[i] = Vector3::new(
+            model.mesh.positions[3 * face[i]],
+            model.mesh.positions[3 * face[i] + 1],
+            model.mesh.positions[3 * face[i] + 2],
+        );
     }
     world_coords
 }
 
-fn get_tringle_positions(model: &Model, face: &[usize; 3], width: i32, height: i32) -> [Pos3f; 3] {
-    let mut positions = [Pos3f {
-        x: 0.0,
-        y: 0.0,
-        z: 0.0,
-    }; 3];
+fn get_tringle_positions(
+    model: &Model,
+    face: &[usize; 3],
+    width: i32,
+    height: i32,
+) -> [Vector3<f32>; 3] {
+    let mut positions = [Vector3::new(0.0, 0.0, 0.0); 3];
     for i in 0..3 {
-        positions[i] = Pos3f {
-            x: (model.mesh.positions[3 * face[i]] + 1.0) * width as f32 / 2.0,
-            y: (model.mesh.positions[3 * face[i] + 1] + 1.0) * height as f32 / 2.0,
-            z: (model.mesh.positions[3 * face[i] + 2] + 1.0),
-        };
+        positions[i] = Vector3::new(
+            (model.mesh.positions[3 * face[i]] + 1.0) * width as f32 / 2.0,
+            (model.mesh.positions[3 * face[i] + 1] + 1.0) * height as f32 / 2.0,
+            (model.mesh.positions[3 * face[i] + 2] + 1.0),
+        );
     }
     positions
 }
 
-fn get_triangle_tex_coords(tex_coords: &[f32], tex_index: &[u32], index: usize) -> [Pos3f; 3] {
-    let mut triangle_tex_coords = [Pos3f {
-        x: 0.0,
-        y: 0.0,
-        z: 0.0,
-    }; 3];
+fn get_triangle_tex_coords(
+    tex_coords: &[f32],
+    tex_index: &[u32],
+    index: usize,
+) -> [Vector3<f32>; 3] {
+    let mut triangle_tex_coords = [Vector3::new(0.0, 0.0, 0.0); 3];
     for i in 0..3 {
-        triangle_tex_coords[i] = Pos3f {
-            x: tex_coords[2 * tex_index[3 * index + i] as usize],
-            y: tex_coords[2 * tex_index[3 * index + i] as usize + 1],
-            z: 0.0,
-        };
+        triangle_tex_coords[i] = Vector3::new(
+            tex_coords[2 * tex_index[3 * index + i] as usize],
+            tex_coords[2 * tex_index[3 * index + i] as usize + 1],
+            0.0,
+        );
     }
     triangle_tex_coords
 }
 
 pub fn triangle(
-    pts: &[Pos3f; 3],
+    pts: &[Vector3<f32>; 3],
     image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>,
     z_buffer: &mut [f32],
     tex_image: &ImageBuffer<Rgb<u8>, Vec<u8>>,
-    triangle_tex_coords: &[Pos3f; 3],
+    triangle_tex_coords: &[Vector3<f32>; 3],
     intensity: f32,
 ) {
-    let mut bboxmin = Pos2i {
-        x: (image.width() - 1) as i32,
-        y: (image.height() - 1) as i32,
-    };
+    let mut bboxmin = Vector2::new((image.width() - 1) as i32, (image.height() - 1) as i32);
 
-    let mut bboxmax = Pos2i { x: 0, y: 0 };
+    let mut bboxmax = Vector2::new(0, 0);
 
-    let clamp = Pos2i {
-        x: (image.width() - 1) as i32,
-        y: (image.height() - 1) as i32,
-    };
+    let clamp = Vector2::new(image.width() as i32 - 1, image.height() as i32 - 1);
 
     for p in pts.iter().take(3) {
         // FIXME: avoid to truncate to i32
@@ -217,7 +149,7 @@ pub fn triangle(
 
     for x in bboxmin.x..bboxmax.x {
         for y in bboxmin.y..bboxmax.y {
-            let bc_screen = barycentric(pts, Pos2i { x, y });
+            let bc_screen = barycentric(pts, Vector2::new(x as f32, y as f32));
 
             if bc_screen.x < 0.0 || bc_screen.y < 0.0 || bc_screen.z < 0.0 {
                 continue;
@@ -227,14 +159,14 @@ pub fn triangle(
 
             if z_buffer[(x + y * image.width() as i32) as usize] < z {
                 z_buffer[(x + y * image.width() as i32) as usize] = z;
-                let uv = Pos2f {
-                    x: triangle_tex_coords[0].x * bc_screen.x
+                let uv = Vector2::new(
+                    triangle_tex_coords[0].x * bc_screen.x
                         + triangle_tex_coords[1].x * bc_screen.y
                         + triangle_tex_coords[2].x * bc_screen.z,
-                    y: triangle_tex_coords[0].y * bc_screen.x
+                    triangle_tex_coords[0].y * bc_screen.x
                         + triangle_tex_coords[1].y * bc_screen.y
                         + triangle_tex_coords[2].y * bc_screen.z,
-                };
+                );
 
                 min_u = min_u.min(uv.x);
                 min_v = min_v.min(uv.y);
@@ -262,6 +194,7 @@ pub fn render_obj(
     tex_image: &ImageBuffer<Rgb<u8>, Vec<u8>>,
 ) {
     let mut z_buffer = vec![f32::MIN; (image.width() * image.height()) as usize];
+
     for i in 0..model.mesh.indices.len() / 3 {
         let face: [usize; 3] = [
             model.mesh.indices[3 * i] as usize,
@@ -273,22 +206,18 @@ pub fn render_obj(
 
         let world_coords = get_world_coords(model, &face);
 
-        let _n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+        let _n = (world_coords[2] - world_coords[0]).cross(&(world_coords[1] - world_coords[0]));
         let n = _n.normalize();
 
-        let light_dir = Pos3f {
-            x: 0.0,
-            y: 0.0,
-            z: -1.0,
-        };
+        let light_dir = Vector3::new(0.0, 0.0, -1.0);
 
-        let intensity = n.dot(light_dir);
+        let intensity = n.dot(&light_dir);
 
         if intensity <= 0.0 {
             continue;
         }
 
-        let triangle_tex_coords: [Pos3f; 3] =
+        let triangle_tex_coords: [Vector3<f32>; 3] =
             get_triangle_tex_coords(&model.mesh.texcoords, &model.mesh.texcoord_indices, i);
 
         triangle(
